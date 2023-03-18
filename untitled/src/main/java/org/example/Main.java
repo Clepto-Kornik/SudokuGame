06d9c9dd7f095d81a1609bd5d01923adba48e53a
236536 Dominik Goczał <236536@edu.p.lodz.pl>
2023-03-18 22:33:41 +0100

    Task 1
    Sudoku is done, no tests are added yet

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import sudoku.SudokuClass;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SudokuClass sudoku = new SudokuClass();
+        sudoku.fillBoard();
+    }
+}
