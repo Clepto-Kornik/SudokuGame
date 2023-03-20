11412f71355e2c45296427f36fedf8ca3e3c5d76
236536 Dominik Goczał <236536@edu.p.lodz.pl>
2023-03-20 19:49:33 +0100

    added tests to task1
    added tests to task1, working properly

--- untitled/src/test/java/sudoku/SudokuClassTest.java
+++ untitled/src/test/java/sudoku/SudokuClassTest.java
@@ -6,6 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class SudokuClassTest {
 
     @Test
-    void fillBoard() {
+    public void CompareSudoku() {
+        SudokuClass sudoku1 = new SudokuClass();
+        SudokuClass sudoku2 = new SudokuClass();
+        sudoku1.fillBoard();
+        sudoku2.fillBoard();
+        assertNotSame(sudoku1, sudoku2);
     }
 }
