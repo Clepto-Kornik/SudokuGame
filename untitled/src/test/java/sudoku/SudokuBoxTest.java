ffe877b8229a1d667c2d8b4752563811f5ca390c
klima7 <github@mail.ukasz.com>
2023-05-13 13:35:56 +0200

    Add cloneable to SudokuBox

--- untitled/src/test/java/sudoku/SudokuBoxTest.java
+++ untitled/src/test/java/sudoku/SudokuBoxTest.java
@@ -2,23 +2,18 @@ package sudoku;
 
 import org.junit.jupiter.api.Test;
 import sudoku.board.SudokuField;
+import sudoku.group.SudokuBox;
+import sudoku.group.SudokuColumn;
 import sudoku.group.SudokuRow;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SudokuBoxTest {
 
     @Test
     public void testVerifyReturnsTrueForValidBox() {
-        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        SudokuField[] fields = new SudokuField[9];
-        for(int i=0; i<9; i++) {
-            fields[i] = new SudokuField(values[i]);
-        }
-
-        SudokuRow box = new SudokuRow(fields);
+        SudokuBox box = createSudokuBox();
         assertTrue(box.verify());
     }
 
@@ -31,7 +26,39 @@ class SudokuBoxTest {
             fields[i] = new SudokuField(values[i]);
         }
 
-        SudokuRow box = new SudokuRow(fields);
+        SudokuBox box = new SudokuBox(fields);
         assertFalse(box.verify());
+    }
+
+    @Test
+    public void testCloneHasProperValue() {
+        SudokuBox row = createSudokuBox();
+        SudokuBox clone = row.clone();
+
+        for(int i=0; i<9; i++) {
+            assertEquals(row.getValue(i), clone.getValue(i));
+        }
+    }
+
+    @Test
+    public void testCloneReturnsNewObjects() {
+        SudokuBox row = createSudokuBox();
+        SudokuBox clone = row.clone();
+
+        int oldValue = row.getValue(0);
+        row.setValue(0, 5);
+
+        assertEquals(clone.getValue(0), oldValue);
+    }
+
+    private SudokuBox createSudokuBox() {
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        SudokuField[] fields = new SudokuField[9];
+        for(int i=0; i<9; i++) {
+            fields[i] = new SudokuField(values[i]);
+        }
+
+        return new SudokuBox(fields);
     }
 }
